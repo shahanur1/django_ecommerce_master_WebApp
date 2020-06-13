b1ca0e8bfdefefb238efa6b9bd0f5f706f38ed99
Shahanur Islam Shagor <65355847+Shahanur1231@users.noreply.github.com>
2020-06-13 22:15:23 +0600

    first commit

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Shagor\Desktop\project\django_ecommerce_master\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
